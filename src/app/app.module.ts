d96919c31de966874f076780607d5d112872be71
Olexiy Zavalniuk <83496121+OlexiyZavalniuk@users.noreply.github.com>
2023-03-04 22:18:57 +0200

    background image on page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { SingInComponent } from './components/sing-in/sign-in.component';
 
 @NgModule({
-  declarations: [AppComponent, MainPageComponent],
+  declarations: [AppComponent, MainPageComponent, SingInComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
